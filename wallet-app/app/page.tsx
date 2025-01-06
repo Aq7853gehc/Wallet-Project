db10c378dcf01b3c3570006a174632ee46be83b0
Aq7853gehc <aquib7583@gmail.com>
2025-01-06 22:04:12 +0530

    the things i want not done but still wallet connect

---
+++ wallet-app/app/page.tsx
@@ -0,0 +1,12 @@
+import { WalletConnect } from '@/components/wallet-connect'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <WalletConnect/>
+    </div>
+  )
+}
+
+export default Home
